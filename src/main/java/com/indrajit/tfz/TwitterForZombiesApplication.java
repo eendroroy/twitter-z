c7717325915f52ea1599d53cfbb297289e6d98b7
indrajit <eendroroy@gmail.com>
2017-06-17 23:19:53 +0600

    spring application

---
+++ src/main/java/com/indrajit/tfz/TwitterForZombiesApplication.java
@@ -0,0 +1,12 @@
+package com.indrajit.tfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterForZombiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterForZombiesApplication.class, args);
+	}
+}
