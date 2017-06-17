a7555a982a784cf6e5c5be46f1d20ddba1d91861
indrajit <eendroroy@gmail.com>
2017-06-18 03:19:16 +0600

    dummy tests

---
+++ src/test/java/com/indrajit/tfz/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.indrajit.tfz.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author indrajit
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+    @Test
+    public void login() throws Exception {
+    }
+
+    @Test
+    public void logoutPage() throws Exception {
+    }
+
+    @Test
+    public void registration() throws Exception {
+    }
+
+    @Test
+    public void createNewUser() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/indrajit/tfz/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.indrajit.tfz.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author indrajit
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Test
+    public void findUserByEmail() throws Exception {
+    }
+
+    @Test
+    public void findUserByUserName() throws Exception {
+    }
+
+    @Test
+    public void saveUser() throws Exception {
+    }
+
+}
