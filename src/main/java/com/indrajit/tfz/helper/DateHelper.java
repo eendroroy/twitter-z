c1e3d2b570000dba0ff94d20818d9c3379309342
indrajit <eendroroy@gmail.com>
2017-06-18 18:03:00 +0600

    date jade helper

---
+++ src/main/java/com/indrajit/tfz/helper/DateHelper.java
@@ -0,0 +1,21 @@
+package com.indrajit.tfz.helper;
+
+import com.domingosuarez.boot.autoconfigure.jade4j.JadeHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author indrajit
+ */
+
+@JadeHelper("dateHelper")
+public class DateHelper {
+    public String format(Date date){
+        if(date == null){
+            return "[Not Given]";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(date);
+    }
+}
